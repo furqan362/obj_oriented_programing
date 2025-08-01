4bce2e746e6484a0869ca2927193aeb6fb938036
furqan362 <furqanch472@gmail.com>
2025-08-01 02:45:19 -0700

    change_2

--- csv_ex.py
+++ csv_ex.py
@@ -1,4 +1,6 @@
-class item:
+import csv
+
+class Item:
     all=[]
     pay_rate=0.8
     def __init__(self,name:str,price:int,quantity=0):
@@ -8,25 +10,29 @@ class item:
         self.name=name
         self.price=price
         self.quantity=quantity
-        item.all.append(self)
+        Item.all.append(self)
+        
     def calculate_total_price(self):
         return self.quantity*self.price
     
     def apply_discount(self):
         self.price=self.price*self.pay_rate
+        
     # it prints items isted of their instances or location
     def __repr__(self):
         return f"item('{self.name}','{self.price},'{self.quantity}')"
+    
+    @classmethod
+    def instance_from_csv(cls):
+        with open('item.csv','r') as f:
+            reader=csv.DictReader(f)
+            items=list(reader)
         
-item1 = item("Phone", 100, 1)
-item2 = item("Laptop", 1000, 3)
-item3 = item("Cable", 10, 5)
-item4 = item("Mouse", 50, 5)
-item5 = item("Keyboard", 75, 5)
-print(item.all)
-
-# for instance in item.all:
-#     print(instance.name)
-
-# print(item.__dict__) #all atributes for class lavel
-# print(item1.__dict__)#all atributes for instance  lavel
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=float(item.get('price')),
+                quantity=int(item.get('quantity'))
+            )
+Item.instance_from_csv()
+print(Item)
